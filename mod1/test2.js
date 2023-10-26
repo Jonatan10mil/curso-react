e67b3201c3085664e96bb8173dc5b1ca9a44fdf8
Pepe <omjonatan222@gmail.com>
2023-10-26 10:44:19 -0400

    Subiendo laboratorio 1

---
+++ mod1/test2.js
@@ -0,0 +1,13 @@
+function mostrarNombre() {
+	return 'Marcelo';
+}
+
+console.log(mostrarNombre());
+
+const mostrarNombre2 = () => {
+	return 'Marcelo2';
+}
+console.log(mostrarNombre2());
+
+const mostrarNombre3 = () => 'Marcelo3';
+console.log(mostrarNombre3());
